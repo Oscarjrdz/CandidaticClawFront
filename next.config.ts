808b5fa3d1bf447a1b8b037e2de01cfe2e09dcd4
Oscar Rodriguez <oscar@Mac.izzigo.tv>
2026-03-29 08:59:07 -0600

    fix: hardcode ssl link directly

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/vps/:path*",
-        destination: "http://64.23.180.202:3000/api/:path*",
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
